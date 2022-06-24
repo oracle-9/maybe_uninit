f6b535510db7df1c4d923ebe7c5c7acf08f03401
oracle <oracle-main@pm.me>
2022-06-24 04:50:40 +0100

    Create maybe_uninit.hpp

---
+++ maybe_uninit.hpp
@@ -0,0 +1,98 @@
+#ifndef MAYBE_UNINIT_INCLUDED
+#define MAYBE_UNINIT_INCLUDED
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+#ifndef MAYBE_UNINIT_NAMESPACE_NAME
+#define MAYBE_UNINIT_NAMESPACE_NAME mem
+#endif
+
+namespace MAYBE_UNINIT_NAMESPACE_NAME {
+
+struct default_construct_tag_t{} inline constexpr default_construct_tag;
+
+template <typename T>
+    requires std::is_object_v<T>
+union maybe_uninit {
+  private:
+    struct unit_t{} m_unit;
+    T m_t;
+
+  public:
+    constexpr maybe_uninit() : m_unit{} {}
+
+    explicit maybe_uninit(default_construct_tag_t)
+        noexcept(std::is_nothrow_default_constructible_v<T>)
+        requires std::is_default_constructible_v<T>
+        : m_unit{}
+    {
+        default_construct();
+    }
+
+    template <typename... Args>
+    explicit constexpr maybe_uninit(Args&&... args)
+        noexcept(std::is_nothrow_constructible_v<T, Args...>)
+        requires std::is_constructible_v<T, Args...>
+    {
+        emplace_construct(std::forward<Args>(args)...);
+    }
+
+    void default_construct()
+        noexcept(std::is_nothrow_default_constructible_v<T>)
+        requires std::is_default_constructible_v<T>
+    {
+        new (std::addressof(m_t)) T;
+    }
+
+    template <typename... Args>
+    constexpr void emplace_construct(Args&&... args)
+        noexcept(std::is_nothrow_constructible_v<T, Args...>)
+        requires std::is_constructible_v<T, Args...>
+    {
+        std::construct_at(std::addressof(m_t), std::forward<Args>(args)...);
+    }
+
+    [[nodiscard]]
+    constexpr T* ptr() noexcept {
+        return std::addressof(m_t);
+    }
+
+    [[nodiscard]]
+    constexpr T const* ptr() const noexcept {
+        return std::addressof(m_t);
+    }
+
+    [[nodiscard]]
+    constexpr T&& assume_init() && noexcept {
+        return std::move(m_t);
+    }
+
+    [[nodiscard]]
+    constexpr T& assume_init() & noexcept {
+        return m_t;
+    }
+
+    [[nodiscard]]
+    constexpr T const& assume_init() const& noexcept {
+        return m_t;
+    }
+
+    constexpr void destroy()
+        noexcept(std::is_nothrow_destructible_v<T>)
+        requires std::is_destructible_v<T>
+    {
+        if constexpr (not std::is_trivially_destructible_v<T>) {
+            std::destroy_at(std::addressof(m_t));
+        }
+    }
+
+    constexpr ~maybe_uninit() {};
+
+    ~maybe_uninit() requires std::is_trivially_destructible_v<T> = default;
+};
+
+} // namespace MAYBE_UNINIT_NAMESPACE_NAME
+
+#endif // MAYBE_UNINIT_INCLUDED
