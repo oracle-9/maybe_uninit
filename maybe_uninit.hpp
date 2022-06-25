2ade55b85b58fd6deeb561a854ab6ee3c104ec17
oracle <oracle-main@pm.me>
2022-06-25 21:20:24 +0100

    Improve type deduction and diagnostics

--- maybe_uninit.hpp
+++ maybe_uninit.hpp
@@ -9,6 +9,14 @@
 #define MAYBE_UNINIT_NAMESPACE_NAME mem
 #endif
 
+#if defined __GNUC__ or defined __clang__
+#   define MAYBE_UNINIT_UNREACHABLE() __builtin_unreachable()
+#elif defined _MSC_VER
+#   define MAYBE_UNINIT_UNREACHABLE() __assume(false)
+#else
+#   define MAYBE_UNINIT_UNREACHABLE() for (;;)
+#endif
+
 namespace MAYBE_UNINIT_NAMESPACE_NAME {
 
 struct default_init_tag_t{} inline constexpr default_init_tag{};
@@ -136,47 +144,63 @@ template <typename Arg>
 maybe_uninit(Arg&&) -> maybe_uninit<std::remove_cvref_t<Arg>>;
 
 template <typename T>
-constexpr maybe_uninit<T> uninit()
-    noexcept(noexcept(maybe_uninit<T>()))
-{
+constexpr maybe_uninit<T> uninit() noexcept {
     return maybe_uninit<T>();
 }
 
 template <typename T>
 inline maybe_uninit<T> default_init()
-    noexcept(noexcept(maybe_uninit<T>(default_init_tag)))
+    noexcept(std::is_nothrow_default_constructible_v<T>)
 {
-    return maybe_uninit<T>(default_init_tag);
+    static_assert(
+        std::is_default_constructible_v<T>,
+        "type must be default constructible"
+    );
+    if constexpr (std::is_default_constructible_v<T>) {
+        return maybe_uninit<T>(default_init_tag);
+    }
+    MAYBE_UNINIT_UNREACHABLE();
 }
 
 template <typename T>
 constexpr maybe_uninit<T> init()
-    noexcept(noexcept(maybe_uninit<T>(value_init_tag)))
+    noexcept(std::is_nothrow_default_constructible_v<T>)
 {
-    return maybe_uninit<T>(value_init_tag);
+    static_assert(
+        std::is_default_constructible_v<T>,
+        "type must be default constructible"
+    );
+    if constexpr (std::is_default_constructible_v<T>) {
+        return maybe_uninit<T>(value_init_tag);
+    }
+    MAYBE_UNINIT_UNREACHABLE();
 }
 
 template <typename T>
 constexpr maybe_uninit<std::remove_cvref_t<T>> init(T&& t)
-    noexcept(noexcept(maybe_uninit(std::forward<T>(t))))
+    noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<T>, T>)
 {
+    using value_type = std::remove_cvref_t<T>;
     static_assert(
-        not std::is_same_v<default_init_tag_t, T>
-            and not std::is_same_v<value_init_tag_t, T>,
+        not std::is_same_v<default_init_tag_t, value_type>
+            and not std::is_same_v<value_init_tag_t, value_type>,
         "using a tag type as a parameter is not allowed"
     );
     static_assert(
-        std::is_constructible_v<T, T>,
+        std::is_constructible_v<value_type, T>,
         "type must be constructible from itself"
     );
-    return mem::maybe_uninit<T>(std::forward<T>(t));
+    if constexpr (std::is_constructible_v<value_type, T>) {
+        return mem::maybe_uninit<value_type>(std::forward<T>(t));
+    }
+    MAYBE_UNINIT_UNREACHABLE();
 }
 
 template <typename T, typename Arg, typename... Args>
 constexpr maybe_uninit<T> init(Arg&& arg, Args&&... args)
-    noexcept(noexcept(
-        maybe_uninit<T>(std::forward<Arg>(arg), std::forward<Args>(args)...)
-    ))
+    noexcept(
+        std::is_nothrow_constructible_v<std::remove_cvref_t<T>, Arg, Args...>
+    )
 {
     static_assert(
         not std::is_same_v<default_init_tag_t, std::remove_cvref_t<Arg>>
@@ -187,13 +211,22 @@ constexpr maybe_uninit<T> init(Arg&& arg, Args&&... args)
             >,
         "using a tag type as a parameter is not allowed"
     );
-
-    return mem::maybe_uninit<T>(
-        std::forward<Arg>(arg),
-        std::forward<Args>(args)...
+    using value_type = std::remove_cvref_t<T>;
+    static_assert(
+        std::is_constructible_v<value_type, Arg, Args...>,
+        "type must be constructible from the provided arguments "
     );
+    if constexpr (std::is_constructible_v<value_type, Arg, Args...>) {
+        return mem::maybe_uninit<value_type>(
+            std::forward<Arg>(arg),
+            std::forward<Args>(args)...
+        );
+    }
+    MAYBE_UNINIT_UNREACHABLE();
 }
 
 } // namespace MAYBE_UNINIT_NAMESPACE_NAME
+
+#undef MAYBE_UNINIT_UNREACHABLE
 
 #endif // MAYBE_UNINIT_INCLUDED
